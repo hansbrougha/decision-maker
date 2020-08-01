55c2abd9845c2c27be9467fc27412481b20a6d38
Jacob Walton <jawalton6616@gmail.com>
2020-08-01 13:34:46 -0500

    added JSON/Mongo database structure

--- db/schema.sql
+++ db/schema.sql
@@ -1,37 +1,78 @@
 CREATE DATABASE decision_db;
 USE decision_db;
 
-CREATE TABLE users
-(
+CREATE TABLE users (
 	'user_id' int NOT NULL AUTO_INCREMENT,
-	'username' varchar(20) NOT NULL,
+    'name' varchar(30) NOT NULL,
+	'username' varchar(20) NOT NULL UNIQUE,
 	'password' varchar(20) NOT NULL,
-	PRIMARY KEY (id)
-);
+    'logged_in' BOOLEAN DEFAULT false,
+	PRIMARY KEY (user_id),
+    );
 
-CREATE TABLE polls
-(
-	'poll_id' int NOT NULL AUTO_INCREMENT,
+CREATE TABLE polls (
+    'user_id' int NOT NULL,
+	'poll_id' int PRIMARY NOT NULL AUTO_INCREMENT,
 	'question' text NOT NULL,
-    'created_at' TIMESTAMP DEFAULT NOT NULL
-	PRIMARY KEY (id)
+    -- 'poll_type' varchar(55) NOT NULL DEAULT "General",
+    'created_at' timestamp DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
-
 
 CREATE TABLE options (
-	`poll_id` int(11) NOT NULL,
-    `option_id` int(11) NOT NULL,
+	`poll_id` int(11) NOT NULL PRIMARY,
+    `option_id` int(11) NOT NULL PRIMARY,
 	`option_text` text NOT NULL,
-	PRIMARY KEY ('poll_id','option_id')
-)
+);
 
 CREATE TABLE votes (
 	`poll_id` int(11) NOT NULL ,
 	`option_id` int(11) NOT NULL,
 	`user_id` text NOT NULL,
-)
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (option_id) REFERENCES options (option_id),
+    FOREIGN KEY (poll_id) REFERENCES polls (poll_id)
+);
 
-ALTER TABLE `votes`
-  ADD KEY `poll_id` (`poll_id`),
-  ADD KEY `option_id` (`option_id`),
-  ADD KEY `user_id` (`user_id`);
+
+
+-- after we create randomizer and rps stuff
+
+CREATE TABLE decisions(   
+    'decision_type' varchar(30) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+
+
+
+-------------------------JSON-----------------------
+
+users:{
+    userID:random id generator funct
+    name:"",
+    username:"",
+    pswd:"",
+    isLoggedIn:boolean
+    polls:[{
+        pollID:random id generator funct, --will be used in route to display/share specific poll
+        pollTitle:"",
+        pollOptions:[{
+            optiontext:"", --stores option
+            optionVal:number --stores votes
+            },
+            {
+            optiontext:"",
+            optionVal:number
+            },
+            {
+            optiontext:"",
+            optionVal:number
+            },
+            {
+            optiontext:"",
+            optionVal:number
+            }]
+    }]
+}
+
